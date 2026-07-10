c716e92b810094114d08b6675ab085205314af62
MikeSydo <mikesydocompan@gmail.com>
2026-07-10 19:34:54 +0300

    Created module myapp with unit tests

---
+++ myapp/app.py
@@ -0,0 +1,11 @@
+from types import SimpleNamespace
+
+def add(a, b):
+    """Adds two numbers"""
+    return a + b
+
+def subtract(a, b):
+    """Subtracts two numbers"""
+    return a - b
+
+app = SimpleNamespace(add=add, subtract=subtract)
